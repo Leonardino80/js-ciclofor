784adfe7869b40a2d49951a5e0d160c8bb54ce82
Leonardino80 <gilgamesh2005@gmail.com>
2019-04-23 03:18:10 +0200

    corretto email

--- email/main.js
+++ email/main.js
@@ -6,16 +6,15 @@ var email_d = ['mail1', 'mail2', 'mail3', 'mail4'];
 
 // ricerca database (si assume che nn c'è come variabile falsa)
 var hasEmail = false;
-// facciamo partire il ciclo
-for (var i = 0; i<email_d.length; i++)
-{
-  if (email_d[i]===email_a){
-  hasEmail = true;}
+for (var i = 0; i<email_d.length; i++) {
+    if (email_d[i]===email_a){
+ hasEmail = true;
+break;}
 }
-if (hasEmail = true){
-alert('email presente');
+if (hasEmail){
+alert("email presente");
 } else {
-alert('email non presente');
+alert("email non presente");
 }
 
 // creato variabile (falsa) prima del ciclo per evitare una risposta ad ogni entry dell'array ed eseguire il ciclo una sola volta.
